f160342c3133cdd41691c5978154437da9f96c2a
Jant <63055012+HamnYce@users.noreply.github.com>
2025-08-19 00:14:42 +0300

    add clangd

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,16 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "lua_ls", "gopls", "ts_ls", "marksman", "nim_langserver" }
+local servers = {
+  "html",
+  "cssls",
+  "lua_ls",
+  "gopls",
+  "ts_ls",
+  "marksman",
+  "nim_langserver",
+  "clangd",
+}
+
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
