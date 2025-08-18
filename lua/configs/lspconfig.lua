0b138a909dbe2381ae96c4deeff9db160192064a
HamnYce <Hamadboa@proton.me>
2025-08-18 12:49:14 +0000

    update lsps

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "lua_ls", "gopls", "ts_ls", "marksman", "nimls" }
+local servers = { "html", "cssls", "lua_ls", "gopls", "ts_ls", "marksman", "nim_langserver" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
